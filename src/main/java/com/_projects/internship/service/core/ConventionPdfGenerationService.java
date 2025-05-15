63e6caa281d6a52cf042f9f610790713388f87fd
Hassan <hassanmahamatdogo@gmail.com>
2025-05-15 14:35:51 +0100

    Configuration de MinIo

--- src/main/java/com/_projects/internship/service/core/ConventionPdfGenerationService.java
+++ src/main/java/com/_projects/internship/service/core/ConventionPdfGenerationService.java
@@ -28,7 +28,7 @@ public class ConventionPdfGenerationService {
      * Génère un document PDF pour une convention de stage
      * @param convention L'entité convention
      * @return Le contenu du PDF sous forme de tableau d'octets
-     * @throws RuntimeException si une erreur survient lors de la génération du PDF
+     * @throws PdfGenerationException si une erreur survient lors de la génération du PDF
      */
     public byte[] generateConventionPdf(Convention convention) {
         try (PDDocument document = new PDDocument();
