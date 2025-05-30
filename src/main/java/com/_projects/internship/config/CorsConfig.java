40ed95dd068be24bfbc8a73ff640f4c81f58743a
loic.k <loickenmoe1@gmail.com>
2025-05-30 15:51:17 +0100

    feat: PreAuthorize update in ApplicationController for the endpoint @GetMapping("/{id}/bundle") and CoreConfig/SecurityConfig update

--- src/main/java/com/_projects/internship/config/CorsConfig.java
+++ src/main/java/com/_projects/internship/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "http://localhost:4200/", "http://127.0.0.1:4200")
+                .allowedOrigins("http://localhost:4200", "http://localhost:4200/", "http://127.0.0.1:4200","http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
